c4a5253db5cb27c05ce2935e86b43b272246a206
KaiserChan <chankaiserkk@gmail.com>
2017-08-09 15:20:43 -0400

    Created exercise6 - Completed with temperature conversion method that converts fahrenheit into celsius

---
+++ exercise6.rb
@@ -0,0 +1,11 @@
+puts "What is the current temerature in Fahrenheit?"
+
+user_input = gets.chomp.to_i
+
+def temp_conversion(fahrenheit)
+  ((fahrenheit - 32) * (5.0/9))
+end
+
+celsius = temp_conversion(user_input)
+
+puts "It is equivalent to #{celsius} degree celsius"
