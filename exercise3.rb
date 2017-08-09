e510e8b6d400665ab6461b8e3f9ea69e95812233
KaiserChan <chankaiserkk@gmail.com>
2017-08-09 14:34:44 -0400

    Updated exercise3 - Corrected minor error with copies

--- exercise3.rb
+++ exercise3.rb
@@ -5,10 +5,10 @@ user_input = gets.chomp.to_i
 def is_even?(number)
 
   if number == 0
-    puts "You entered ZERO. Its neither positive or negative"
+    puts "You entered ZERO. Its neither odd nor even"
   elsif number % 2 == 0
     puts "This is an even number"
-  else 
+  else
       puts "This is an odd number"
   end
 
