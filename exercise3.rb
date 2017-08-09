3e6ba6411b2adaaa903558650617aa1be00e71b6
KaiserChan <chankaiserkk@gmail.com>
2017-08-09 14:33:49 -0400

    Created exercise3 - Completed with even/odd check method

---
+++ exercise3.rb
@@ -0,0 +1,19 @@
+puts "Enter random number here"
+
+user_input = gets.chomp.to_i
+
+def is_even?(number)
+
+  if number == 0
+    puts "You entered ZERO. Its neither positive or negative"
+  elsif number % 2 == 0
+    puts "This is an even number"
+  else 
+      puts "This is an odd number"
+  end
+
+end
+
+results = is_even?(user_input)
+
+puts results
